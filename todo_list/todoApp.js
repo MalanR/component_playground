8bb36f2b859611022053cae60bbbf7ccd590ae8f
Malan <malanrautenbach777@gmail.com>
2026-08-31 21:15:12 +0200

    start fresh

--- todo_list/todoApp.js
+++
@@ -1,8 +0,0 @@
-import { TaskContainer } from "./components/taskContainer.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.querySelector('.task-container');
-    app.innerHTML = '';
-    const taskContainer = TaskContainer(); // Create a new container element
-    app.appendChild(taskContainer); // Append the new container
-});
